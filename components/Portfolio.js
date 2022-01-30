8d55a4ac949177235c9c8cfb90cb9b7261572bfe
Kay Alvaer <mosarwe@yahoo.co.uk>
2022-01-31 00:00:44 +0100

    added changes

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -17,7 +17,7 @@ export default function Portfolio() {
             </div>
 
             <div className="row d-flex justify-content-around">
-            <Link href="/" >
+
                 <div className="border border-light p-3 ml-4 col-lg-4 serv animateInLeft">
                     <div className="box1">
                         <a href="#home" data-toggle="tooltip" data-animation="58" data-goto="1">
@@ -30,12 +30,15 @@ export default function Portfolio() {
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt doloremque excepturi sit odit impedit, voluptas. Vel, facere nobis, beatae inventore maxime veniam!
                             </p>
+                            <Link href="https://xd.adobe.com/view/895e5434-a851-43af-93aa-d77b347c80f3-dc0d" >
+                                <button className="btn-warning p-2">
+                                    View projects
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
-                </Link>
 
-                <Link href="/" >
                 <div className="border border-light p-3 col-lg-4 serv bounceInUp">
                     <div className="box1">
                         <a href="#home" data-toggle="tooltip" data-animation="58" data-goto="1">
@@ -48,12 +51,15 @@ export default function Portfolio() {
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt doloremque excepturi sit odit impedit, voluptas. Vel, facere nobis, beatae inventore maxime veniam!
                             </p>
+                            <Link href= "https://flowerpowersite.vercel.app/</div>">
+                                <button className="btn-warning p-2">
+                                    View projects
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
-                </Link>
 
-                <Link href="/" >
                 <div className="border border-light p-3 col-lg-4 serv bounceInRight">
                     <div className="box1">
                         <a href="#home" data-toggle="tooltip" data-animation="58" data-goto="1">
@@ -66,10 +72,14 @@ export default function Portfolio() {
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt doloremque excepturi sit odit impedit, voluptas. Vel, facere nobis, beatae inventore maxime veniam!
                             </p>
+                            <Link href="https://xd.adobe.com/view/0394cb37-8758-4bdb-b4c4-c905cbce73f2-56c7/screen/2f4d2cf4-d2b7-448d-9768-6557955cdf0f" >
+                                <button className="btn-warning p-2">
+                                    View projects
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
-                </Link>
 
         </div>
 
